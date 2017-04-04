eeac245f403fd5121a6a165cc1e86a59f92c2564
Michael <michael@ipv6secure.co.uk>
2017-04-04 14:30:37 +0000

    Updated simple-express to handle form submission.

--- NodeJS/simple-express.js
+++ NodeJS/simple-express.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(express.static('express-site'));
 
@@ -14,10 +17,24 @@ app.get('/listusers', function (req, res)
    res.sendFile( __dirname + "/express-site/" + "users.html" );
 })
 
+app.post('/adduser', urlencodedParser, function (req, res) 
+{
+   // Prepare output in JSON format
+   response = {
+      first_name:req.body.firstName,
+      last_name:req.body.secondName,
+	  user_name:req.body.userName,
+	  user_email:req.body.emailAddress
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
 var server = app.listen(8090, function () 
 {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
 })
+
 
