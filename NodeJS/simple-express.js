b7728a001938d9548c3ea8724102302f02970760
Michael <michael@ipv6secure.co.uk>
2017-04-06 11:38:48 +0000

    Updated Node.js Express code.

--- NodeJS/simple-express.js
+++ NodeJS/simple-express.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var fs = require("fs");
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
@@ -17,18 +18,24 @@ app.get('/listusers', function (req, res)
    res.sendFile( __dirname + "/express-site/" + "users.html" );
 })
 
+
 app.post('/adduser', urlencodedParser, function (req, res) 
 {
-   // Prepare output in JSON format
-   response = {
-      first_name:req.body.firstName,
-      last_name:req.body.secondName,
-	  user_name:req.body.userName,
-	  user_email:req.body.emailAddress
-   };
-   console.log(response);
-   res.end(JSON.stringify(response));
-})
+	response = {
+    firstName:req.body.firstName,
+    secondName:req.body.secondName,
+	user_name:req.body.userName,
+	user_email:req.body.emailAddress
+	}
+
+	var userWrite = JSON.stringify(response);
+	var writerStream = fs.createWriteStream('users.json', {flags: 'a'});
+
+	writerStream.write(userWrite);
+	writerStream.end();
+	res.sendFile( __dirname + "/express-site/" + "users.html" );
+});
+
 
 var server = app.listen(8090, function () 
 {
